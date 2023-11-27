5c72a3302287d5532596f9d112b5e0d86de02ad0
osck53 <osck53@OSCARKALUZA6258>
2023-11-27 14:30:59 +0100

    Add project files.

---
+++ ContainerShip/Container.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainerShip
+{
+    public class Container
+    {
+        public int MaximumWeight { get; set; }
+        public int WeightOnContainer { get; set; }
+        public bool Valuable { get; set; }
+        public bool Cooled { get; set; }
+    }
+}
---
+++ ContainerShip/Ship.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainerShip
+{
+    public class Ship
+    {
+        public int Lenght { get; set; }
+        public int Width { get; set; }
+        public int MaximumWeight { get; set; }
+        public int CooledItems { get; set; }
+    }
+}
