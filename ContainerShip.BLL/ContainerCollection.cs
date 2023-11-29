16f53e10238bf7f782d2ef299d3076ea95bc9a9d
osck53 <osck53@OSCARKALUZA6258>
2023-11-29 19:38:40 +0100

    data class

---
+++ ContainerShip.BLL/ContainerCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainerShip.BLL
+{
+    public class ContainerCollection
+    {
+        public List<Container> containers {  get; set; }    
+
+        public List<Container> GetContainers()
+        {
+            containers = new List<Container>();
+
+            containers.Add(new BLL.Container(400, ContainerType.Empty));
+            containers.Add(new BLL.Container(400, ContainerType.Empty));
+            containers.Add(new BLL.Container(1000, ContainerType.Full));
+            containers.Add(new BLL.Container(1275, ContainerType.Full));
+            containers.Add(new BLL.Container(800, ContainerType.Cooled));
+            containers.Add(new BLL.Container(810, ContainerType.Cooled));
+            containers.Add(new BLL.Container(1500, ContainerType.Valuable));
+            containers.Add(new BLL.Container(1477, ContainerType.Valuable));
+
+            return containers;
+        }
+
+
+    }
+}
--- ContainerShip/Program.cs
+++ ContainerShip/Program.cs
@@ -12,20 +12,11 @@ namespace ContainerShip
     {
         static void Main(string[] args)
         {
+            ContainerCollection containers = new ContainerCollection();
             Ship ship = new Ship(1000, 10, 10, 10);
 
-            List<BLL.Container> containers = new List<BLL.Container>();
 
-            containers.Add(new BLL.Container(400, ContainerType.Empty));
-            containers.Add(new BLL.Container(400, ContainerType.Empty));
-            containers.Add(new BLL.Container(1000, ContainerType.Full));
-            containers.Add(new BLL.Container(1275, ContainerType.Full));
-            containers.Add(new BLL.Container(800, ContainerType.Cooled));
-            containers.Add(new BLL.Container(810, ContainerType.Cooled));
-            containers.Add(new BLL.Container(1500, ContainerType.Valuable));
-            containers.Add(new BLL.Container(1477, ContainerType.Valuable));
-
-            ship.AddContainer(containers);
+            ship.AddContainer(containers.GetContainers());
 
             Console.WriteLine(ship.ShipLayout);
             Console.ReadLine();
